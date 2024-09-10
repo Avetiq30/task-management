0489becf5ec00e34321b24929ff9d121f9313f1e
AVETIK <avetik@MacBook-Pro-AVETIK.local>
2024-09-10 08:32:48 -0700

    task

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException, BadRequestException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { Task } from './task.entity';
 import { CreateTaskDto } from './dto/create-task.dto';
-import {UpdateTaskDto} from "./dto/update-task.dto";
+import { UpdateTaskDto } from './dto/update-task.dto';
 
 @Injectable()
 export class TaskService {
@@ -12,24 +12,50 @@ export class TaskService {
         private taskRepository: Repository<Task>,
     ) {}
 
-    findAll(): Promise<Task[]> {
-        return this.taskRepository.find();
+    async findAll(): Promise<Task[]> {
+        const tasks = await this.taskRepository.find();
+        if (!tasks.length) {
+            throw new NotFoundException('No tasks found');
+        }
+        return tasks;
     }
 
-    findOne(id: number): Promise<Task> {
-        return this.taskRepository.findOneBy({ id });
+    async findOne(id: number): Promise<Task> {
+        if (!id) {
+            throw new BadRequestException('Invalid task ID');
+        }
+
+        const task = await this.taskRepository.findOneBy({ id });
+        if (!task) {
+            throw new NotFoundException(`Task with ID ${id} not found`);
+        }
+
+        return task;
     }
 
-    createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+    async createTask(createTaskDto: CreateTaskDto): Promise<Task> {
+        if (!createTaskDto.title || !createTaskDto.assignee) {
+            throw new BadRequestException('Title and assignee are required');
+        }
+
         const task = this.taskRepository.create(createTaskDto);
         return this.taskRepository.save(task);
     }
 
     async updateTask(id: number, updateTaskDto: UpdateTaskDto): Promise<Task> {
+        if (!id) {
+            throw new BadRequestException('Invalid task ID');
+        }
+
         const task = await this.taskRepository.findOneBy({ id });
         if (!task) {
-            throw new Error('Task not found');
+            throw new NotFoundException(`Task with ID ${id} not found`);
         }
+
+        if (!updateTaskDto.title && !updateTaskDto.assignee && !updateTaskDto.status) {
+            throw new BadRequestException('At least one field must be updated');
+        }
+
         Object.assign(task, updateTaskDto);
         return this.taskRepository.save(task);
     }
