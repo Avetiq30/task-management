331d654a40c069a750be62316434eddb6fa7f1b9
avetik <avetiq1992avo@gmail.com>
2025-01-27 02:55:21 -0800

    m

--- dist/task/dto/create-task.dto.d.ts
+++
@@ -1,8 +0,0 @@
-import { TaskPriority } from "../task.enum";
-export declare class CreateTaskDto {
-    title: string;
-    description: string;
-    dueDate: Date;
-    priority: TaskPriority;
-    assignee: string;
-}
